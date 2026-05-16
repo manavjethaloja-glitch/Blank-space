c18de9b230da176b7f0720602def459248873d17
manavjethaloja-glitch <manav.jethaloja@gdgoenkasurat.com>
2026-05-17 02:24:48 +0530

    Create firebase.ts

---
+++ src/firebase.ts
@@ -0,0 +1,17 @@
+import { initializeApp } from "firebase/app";
+import { getFirestore } from "firebase/firestore";
+import { getStorage } from "firebase/storage";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyCOgPyYAIuP-txKO4-4tPdh3tZ08bJ74J4",
+  authDomain: "blank-space-store.firebaseapp.com",
+  projectId: "blank-space-store",
+  storageBucket: "blank-space-store.firebasestorage.app",
+  messagingSenderId: "1001310953860",
+  appId: "1:1001310953860:web:9796a2ad4d760b30197dd1",
+};
+
+const app = initializeApp(firebaseConfig);
+
+export const db = getFirestore(app);
+export const storage = getStorage(app);
